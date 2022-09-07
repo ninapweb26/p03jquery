ffd28ed1c46e92ec657e700bd3e54644e22136af
Nina <112747676+ninapweb26@users.noreply.github.com>
2022-09-08 06:23:43 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $("td").click(function (event) {
-      alert("Kamu klik " + $(this).html() + "!");
-    });
-    $("th").click(function (event) {
-      alert("Kamu klik " + $(this).html() + "!");
-    });
-  });
